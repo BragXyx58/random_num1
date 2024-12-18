7495008098e6dbcc8d814bc26cbcd686d576ec05
BragXyx58 <BragX_yx58@student.itstep.org>
2024-12-18 09:14:58 +0100

    Initial commit

---
+++ random_num.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import random
+
+def generate_random_number():
+    random_number = random.randint(1, 100)
+    result.config(text=f"Случайное число: {random_number}")
+
+root = Tk()
+root.geometry("200x200")
+root.title("Генератор случайных чисел")
+button_generate = Button(root, text="Сгенерировать число", command=generate_random_number).pack()
+result = Label(root, text="Случайное число: ")
+result.pack()
+root.mainloop()
